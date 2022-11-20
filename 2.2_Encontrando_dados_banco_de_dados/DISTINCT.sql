61c3f163e71a3d193066c7485d06b43f5af9316c
Caroline Nunes <nunescaroline905@gmail.com>
2022-11-20 20:20:22 -0300

    UPDATE: exercicios/2.2

---
+++ 2.2_Encontrando_dados_banco_de_dados/DISTINCT.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sakila.actor;
+SELECT first_name, last_name FROM sakila.actor;
+SELECT DISTINCT first_name, last_name FROM sakila.actor;
+
+SELECT DISTINCT Nome, Idade FROM Escola.Estudantes; -- 5LINHAS
+SELECT DISTINCT Nome FROM Escola.Estudantes; -- 4 linhas
+SELECT DISTINCT Idade FROM Escola.Estudantes; -- 4 linhas
