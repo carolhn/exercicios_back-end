8ea3dac13521475ed9c0858bc2151076b421bc74
Caroline Nunes <nunescaroline905@gmail.com>
2022-12-07 15:15:32 -0300

    ADD: exercicio/4.1

---
+++ 4.1_Node.js_Runtime_Assíncrono/script_simples/src/aksName.js
@@ -0,0 +1,21 @@
+const readline = require('readline-sync');
+
+const name = readline.question('Qual seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+console.log(`Hello, ${name}! You are ${age} years old!`);
+
+
+
+
+// console.log('Hello, world!');
+const aksNane = () => readline.question('qual nome voce quer pesquisar?') // receber um nome do terminal/ question vai printar salvar
+console.log(name);
+
+
+module.exports = aksNane;
+
+
+
+
+
+
