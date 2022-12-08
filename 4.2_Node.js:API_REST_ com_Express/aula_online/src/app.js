c172480dd16a7b3583310cfd0c688383ede887ad
Caroline Nunes <nunescaroline905@gmail.com>
2022-12-08 13:29:37 -0300

    ADD: exercicio/4.2

---
+++ 4.2_Node.js:API_REST_ com_Express/aula_online/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+// app.use(express.json())
+
+// criaçao dos nosso endpoins(o que as pessoa acessao)
+
+app.get('/atividades', (request, response) => {
+
+});
+
+module.exports = app;
---
+++ 4.2_Node.js:API_REST_ com_Express/aula_online/src/server.js
@@ -0,0 +1,4 @@
+const app = require('./app');
+
+app.listen(3002,() => console.log('o pai ta on na porta 3001')); // coloquei o servidor rodar e destinei a porta
+
