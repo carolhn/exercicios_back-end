6a9a6b513f811eb41a835fd7b2c193f3b03d75c4
Caroline Nunes <nunescaroline905@gmail.com>
2022-12-19 14:00:11 -0300

    add: exemplos

--- 4.3_Node.js:Testes_de_Integração/cacau-trybe_aula_ao_vivo/src/cacauTrybe.js
+++ 4.3_Node.js:Testes_de_Integração/cacau-trybe_aula_ao_vivo/src/cacauTrybe.js
@@ -52,6 +52,48 @@ const getChocolatesByBrand = async (brandId) => {
     .filter((chocolate) => chocolate.brandId === brandId);
 };
 
+
+//4 - Implemente o endpoint GET /chocolates/search na aplicação
+const findChocolateByName = async (query) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates
+    .filter((chocolate) => chocolate.name.toLowerCase().includes(query.toLowerCase()));
+};
+
+// 6 - Implemente o endpoint PUT /chocolates/:id na aplicação
+const fs = require('fs').promises;
+const { join } = require('path');
+
+const writeCacauTrybeFile = async (content) => {
+  try {
+    const completePath = join(__dirname, path);
+    await fs.writeFile(completePath, JSON.stringify(content));
+  } catch (e) {
+    console.error('Erro ao salvar o arquivo', e.message);
+    return null;
+  }
+}
+
+const updateChocolate = async (id, update) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  const chocolateToUpdate = cacauTrybe.chocolates.find(
+    (chocolate) => chocolate.id === id,
+  );
+
+  if (chocolateToUpdate) {
+    cacauTrybe.chocolates = cacauTrybe.chocolates.map((chocolate) => {
+        if (chocolate.id === id) return { ...chocolate, ...update };
+        return chocolate;
+      });
+  
+    await writeCacauTrybeFile(cacauTrybe);
+    return { ...chocolateToUpdate, ...update };
+  }
+
+  return false;
+};
+
+
 // metodo que deleta pelo ID
 const deleteChocolate = async (id) => {
   const cacauTrybe = await readCacauTrybeFile();
@@ -77,5 +119,7 @@ module.exports = {
   getChocolatesByBrand,
   readCacauTrybeFile,
   createChocolate,
-  deleteChocolate
+  deleteChocolate,
+  findChocolateByName,
+  updateChocolate,
 };
--- 4.3_Node.js:Testes_de_Integração/cacau-trybe_aula_ao_vivo/src/tests/integration/chocolates.test.js
+++ 4.3_Node.js:Testes_de_Integração/cacau-trybe_aula_ao_vivo/src/tests/integration/chocolates.test.js
@@ -110,6 +110,92 @@ describe('Testando a API Cacau Trybe', function () {
     });
   });
 
+// listar todos os chocolates
+  describe('Usando o método GET em /chocolates/total', function () {
+    it('Retorna a quantidade total de chocolates', async function () {
+      const response = await chai.request(app)
+        .get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal({ totalChocolates: 4 });
+  });
+});
+
+
+// 4 - Crie os testes de integração para o endpoint GET /chocolates/search
+  describe('Usando o método GET em /chocolates/search', function () {
+    it('Retorna os chocolates que contém "Mo" no nome', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/search?name=Mo');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal([
+        {
+          id: 3,
+          name: 'Mon Chéri',
+          brandId: 2,
+        },
+        {
+          id: 4,
+          name: 'Mounds',
+          brandId: 3,
+        },
+      ]);
+    });
+
+    it('Retorna um array vazio ao não encontrar nenhum chocolate', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/search?name=ZZZ');
+
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.deep.equal([]);
+    });
+  });
+
+  // 5 - Crie os testes de integração para o endpoint PUT /chocolates/:id
+
+  describe('Usando o método PUT em /chocolates/:id', function () {
+    it('Atualiza um chocolate existente', async function () {
+      const response = await chai
+        .request(app)
+        .put('/chocolates/1').send({
+        name: 'Mint Pretty Good',
+        brandId: 2,
+      });
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolate).to.deep.equal({
+        id: 1,
+        name: 'Mint Pretty Good',
+        brandId: 2,
+      });
+    });
+      
+    it('Se o chocolate não existe, gera um erro', async function () {
+      const response = await chai
+        .request(app)
+        .put('/chocolates/555').send({
+        name: 'Mint Pretty Good',
+        brandId: 2,
+        });
+      
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.deep.equal({
+        message: 'chocolate not found',
+      });
+    });
+  });
+
+
+
+
+
+
+
+
+
   // deletar chocolates
   describe("Usando o método DELETE em /chocolates/:id", () => {
     it("Remove um chocolate", async () => {
