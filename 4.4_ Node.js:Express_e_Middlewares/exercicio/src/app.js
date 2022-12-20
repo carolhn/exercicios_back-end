2c50b2e2d57cb6f2f5ade3c9bc617f4e5f97e86d
Caroline Nunes <nunescaroline905@gmail.com>
2022-12-20 19:56:05 -0300

    ADD: exercicio1 concluido

--- 4.4_ Node.js:Express_e_Middlewares/exercicio/src/app.js
+++ 4.4_ Node.js:Express_e_Middlewares/exercicio/src/app.js
@@ -5,6 +5,7 @@ const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -12,10 +13,21 @@ app.use(express.json()); //interpretar os conteúdos JSON.
 
 
 //3- Crie um endpoint POST com a rota /activities para adicionar novas atividades. Ela deve:
-app.post('/activities',validateDifficulty, validateRating, validateCreatedAt,validateDescription, validatePrice, validateName,
+app.post('/activities', auth,validateDifficulty, validateRating, validateCreatedAt,validateDescription, validatePrice, validateName,
 (_req, res) => {
 res.status(201).json({ message: `atividade foi cadastrada com sucesso` })
 });
 
+// autenticação de endpoint
+app.post('/signup', (_req, res) => {
+    const { email, password, firstName, phone } = req.body;
+  
+    if ([email, password, firstName, phone].includes(undefined)) {
+      return res.status(401).json({ message: 'Campos ausentes!' });
+    }
+  
+    const token = generateToken();
+    return res.status(200).json({ token });
+  });
 
 module.exports = app;
