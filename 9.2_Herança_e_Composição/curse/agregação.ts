cbc07e3c7c018d8f178ac2c5819085fe1c90469c
Caroline Nunes <nunescaroline905@gmail.com>
2023-02-12 13:23:33 -0300

    feat: adding exercise

---
+++ 9.2_Herança_e_Composição/curse/agregação.ts
@@ -0,0 +1,49 @@
+interface Logger {
+// _Logger_ deve possuir um método `log`, que recebe um parâmetro do tipo _string_ e não retorna nada (_void_).
+    log(param: string): void;
+}
+
+// Crie uma classe _ConsoleLogger_ que implementa _Logger_.
+class ConsoleLogger implements Logger {
+    log(param: string): void {
+        console.log(param);
+    }
+}
+
+// Crie uma classe _ConsoleLogger2_ igual à classe anterior, apenas a título de exemplo.
+class ConsoleLoggers2 implements Logger {
+    log(param: string): void {
+        console.log('loger2', + param);
+    }
+}
+
+// Crie uma interface _Database_.
+interface Database {
+    logger: Logger;                          // _Database_ deve possuir um atributo do tipo `Logger`.
+    save(key: string, value: string): void; // _Database_ deve possuir um método `save`, que recebe dois parâmetros, `key` e `value`, ambos _strings_, e não retorna nada (_void_).
+  }
+  
+
+// Crie uma classe _ExampleDatabase_ que implementa _Database_.
+class ExampleDatabase implements Database {
+    constructor(public logger: Logger = new ConsoleLogger()) { }     //receber o _Logger_ como parâmetro do construtor, e possuir como valor padrão um _ConsoleLogger_.
+  
+    // Dentro do método `save`, chame a função `log` do _Logger_ passado para o database para fazer o log dos parâmetros passados para a `save`.
+    save(key: string, value: string): void {
+      this.logger.log(`Salvando o valor ${value} na chave ${key}`);
+    }
+  }
+
+// Crie um objeto de cada um dos _Loggers_.
+const logger1 = new ConsoleLogger();
+const logger2 = new ConsoleLoggers2();
+
+// Crie três objetos da _ExampleDatabase_, cada um dos dois primeiros recebendo um dos _Loggers_, e o último não recebendo nenhum.
+const database1 = new ExampleDatabase(logger1);
+const database2 = new ExampleDatabase(logger2);
+const database3 = new ExampleDatabase();
+
+// Utilize todos eles para salvar um conteúdo fictício.
+database1.save('chave 1', 'valor 1');
+database2.save('chave 2', 'valor 2');
+database3.save('chave 3', 'valor 3');
